62d7c260a30733838f5090c3f8bc9964df0c8217
Nadeem <nadeem@example.com>
2025-12-24 21:50:07 +0530

    updated

--- src/app/upgrade/welcome/page.jsx
+++ src/app/upgrade/welcome/page.jsx
@@ -1,21 +1,31 @@
 import Script from "next/script";
 import SetupPage from "./SetupPage";
+import MainLayout from "@/components/MainLaout";
+import Hero from "@/components/Hero";
+import QuickEasySection from "@/components/QuickEasySection";
+import UltimatePCCare from "@/components/UltimatePCCare";
+import WorkWithDevices from "@/components/WorkWithDevices";
+import DriverPack from "@/components/DriverPack";
+import PricingPackages from "@/components/PricingSection";
+import FAQList from "@/components/FaqList";
+import Talk from "@/components/Talk";
+import CustomersReview from "@/components/CostumersReview";
 
-export const metadata = {
-  title: "HP - Professional Printer Download Driver & Setup Solutions",
-  description:
-    "Expert printer services including installation, troubleshooting, maintenance, and 24/7 technical assistance for all printer brands.",
-  icons: {
-  icon: "/hplogo.png",
-},
+// export const metadata = {
+//   title: "HP - Professional Printer Download Driver & Setup Solutions",
+//   description:
+//     "Expert printer services including installation, troubleshooting, maintenance, and 24/7 technical assistance for all printer brands.",
+//   icons: {
+//   icon: "/hplogo.png",
+// },
 
-};
+// };
 
 export default function Page() {
   return (
     <>
       {/* ✅ Load Tawk.to script properly */}
-      <Script
+      {/* <Script
         id="tawk-to"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
@@ -31,9 +41,33 @@ export default function Page() {
             })();
           `,
         }}
-      />
+      /> */}
 
-      <SetupPage />
+      {/* <SetupPage /> */}
+      <MainLayout>
+      {/* Hero */}
+      <Hero/>
+    
+
+      {/* Quick & Easy */}
+      <QuickEasySection />
+
+      {/* Ultimate PC Care */}
+      <UltimatePCCare/>
+
+      {/* Works with devices */}
+      <WorkWithDevices/>
+
+
+      {/* Suitable for any computer */}
+      <DriverPack/>
+      <PricingPackages/>
+
+      {/* Contact */}
+      <CustomersReview/>
+      <FAQList/>
+      <Talk/>
+    </MainLayout>
     </>
   );
 }
